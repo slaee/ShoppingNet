8327b9b1927f0993f939c3cc2bb1d920a625eca1
newbietot <nasa.sly14@gmail.com>
2020-10-04 13:30:50 +0800

    htdocs

--- htdocs/database/database_lvl3.php
+++
@@ -1,9 +0,0 @@
-<?php
-// DATABASE LVL 3 ASSUME
-$con = mysqli_connect('localhost', 'root', '', 'test');
-	if (mysqli_connect_errno()){
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-		die();
-		}
-$link = mysqli_connect("localhost", "root", "", "test");
-?>
--- htdocs/driver_profile.php
+++
@@ -1,1 +0,0 @@
-<?php include 'profile.php'; ?>
